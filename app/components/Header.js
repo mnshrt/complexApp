a9556334d6bf1579ed9216c8196a9fea0f45c1cb
mnshrt <mnshrt2292@gmail.com>
2022-10-31 10:04:31 +0530

    dynamic header content on login status change

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,7 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 import {Link} from 'react-router-dom'
 import HeaderLoggedOut from "./HeaderLoggedOut"
+import HeaderLoggedIn from "./HeaderLoggedIn"
 function Header() {
+  const [loggedIn, setLoggedIn] = useState(false)
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -10,7 +12,7 @@ function Header() {
             ComplexApp
           </Link>
         </h4>
-           <HeaderLoggedOut/>
+           {loggedIn?<HeaderLoggedIn setLoggedIn={setLoggedIn}/>:<HeaderLoggedOut setLoggedIn={setLoggedIn}/>}
       </div>
     </header>
   )
--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -1,10 +1,26 @@
 import React, { useEffect } from "react"
+import { Link } from "react-router-dom"
 
-function HeaderLoggedIn() {
+function HeaderLoggedIn(props) {
   return (
-    <>
-      
-    </>
+    <div className="flex-row my-3 my-md-0">
+    <Link to="#" className="text-white mr-2 header-search-icon">
+      <i className="fas fa-search"></i>
+    </Link>
+    <span className="mr-2 header-chat-icon text-white">
+      <i className="fas fa-comment"></i>
+      <span className="chat-count-badge text-white"> </span>
+    </span>
+    <Link to="#" className="mr-2">
+      <img className="small-header-avatar" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" />
+    </Link>
+    <Link className="btn btn-sm btn-success mr-2" to="/create-post">
+      Create Post
+    </Link>
+    <button className="btn btn-sm btn-secondary" onClick={()=> props.setLoggedIn(false)}>
+      Sign Out
+    </button>
+  </div>
   )
 }
 
