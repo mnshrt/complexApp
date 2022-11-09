e83971ca330e7710885805a01e70abbdfb335a1f
mnshrt <mnshrt2292@gmail.com>
2022-11-09 19:29:59 +0530

    react tooltip added

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useParams } from "react-router-dom"
 import Axios from "axios"
 import ReactMarkdown from "react-markdown"
+import ReactTooltip from "react-tooltip"
 import Page from "./Page"
 
 function ViewSinglePost() {
@@ -36,8 +37,10 @@ fetchPosts()
       <div className="d-flex justify-content-between">
         <h2>{post.title}</h2>
         <span className="pt-2">
-          <a href="#" className="text-primary mr-2" title="Edit"><i className="fas fa-edit"></i></a>
-          <a className="delete-post-button text-danger" title="Delete"><i className="fas fa-trash"></i></a>
+          <a href="#" data-tip="Edit" data-for="edit" className="text-primary mr-2" ><i className="fas fa-edit"></i></a>
+          <ReactTooltip id="edit" className="custom-tooltip"/>
+          <a data-tip="Delete" data-for="delete" className="delete-post-button text-danger" title="Delete"><i className="fas fa-trash"></i></a>
+          <ReactTooltip id="delete" className="custom-tooltip"/>
         </span>
       </div>
 
